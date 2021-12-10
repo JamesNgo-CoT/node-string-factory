776cd5764ca28e65cac49e98db61078a86558011
jngo2 <james.ngo@toronto.ca>
2021-12-10 14:27:28 -0500

    Update test and readme file

--- test.js
+++ test.js
@@ -1,46 +1,148 @@
-const { exp, expIf, func, tag, table } = require('./index');
+const { exp, expIf, expLoop, func, quote, table, tag } = require('./index');
 
-const content = [
-	tag('!DOCTYPE', { html: null }),
-	tag('html', { lang: 'en' }, [
-		tag('head', {}, [
-			tag('meta', { charset: 'UTF-8' }),
-			tag('meta', { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' }),
-			tag('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }),
-			tag('title', {}, exp('title'))
+////////////////////////////////////////////////////////////////////////////////
+// TAG
+////////////////////////////////////////////////////////////////////////////////
+
+(() => {
+	console.log('\x1b[36mTAG\x1b[0m');
+
+	const content = tag('div', { class: 'class-name' }, [
+		tag('div', {}, [
+			'TEXT',
+			tag('br', {}),
+			'TEXT'
+		])
+	]);
+
+	console.log(content);
+})();
+
+////////////////////////////////////////////////////////////////////////////////
+// TABLE
+////////////////////////////////////////////////////////////////////////////////
+
+(() => {
+	console.log('\x1b[36mTABLE\x1b[0m');
+
+	const content = table({ border: 1, cellpadding: 0, cellspacing: 0 }, 'CAPTION', [
+		tag('tr', {}, [
+			tag('th', { scope: 'col' }, 'HEADER 1'),
+			tag('th', { scope: 'col' }, 'HEADER 2'),
+			tag('th', { scope: 'col' }, 'HEADER 3')
+		])
+	], [
+		tag('tr', {}, [
+			tag('th', { scope: 'row' }, 'HEADER 1'),
+			tag('td', {}, 'DATA 2'),
+			tag('td', {}, 'DATA 3')
 		]),
-		tag('body', {}, [
-			expIf('title', exp('title'), ''),
-			table({}, 'CAPTION', [
-				tag('tr', {}, [
-					tag('th', { scope: 'col' }, 'HEADER 1'),
-					tag('th', { scope: 'col' }, 'HEADER 2'),
-					tag('th', { scope: 'col' }, 'HEADER 3'),
-					tag('th', { scope: 'col' }, 'HEADER 4')
-				])
-			], [
-				tag('tr', {}, [
-					tag('th', { scope: 'row' }, 'DATA 1'),
-					tag('td', {}, 'DATA 2'),
-					tag('td', {}, 'DATA 3'),
-					tag('td', {}, 'DATA 4')
-				]),
-				tag('tr', {}, [
-					tag('th', { scope: 'row' }, 'DATA 1'),
-					tag('td', {}, 'DATA 2'),
-					tag('td', {}, 'DATA 3'),
-					tag('td', {}, 'DATA 4')
-				])
-			], [
-				tag('tr', {}, [
-					tag('td', {}, ''),
-					tag('td', {}, 'FOOTER 2'),
-					tag('td', {}, 'FOOTER 3'),
-					tag('td', {}, 'FOOTER 4')
+		tag('tr', {}, [
+			tag('th', { scope: 'row' }, 'HEADER 1'),
+			tag('td', {}, 'DATA 2'),
+			tag('td', {}, 'DATA 3')
+		])
+	], [
+		tag('tr', {}, [
+			tag('td', {}, ''),
+			tag('td', {}, 'FOOTER 2'),
+			tag('td', {}, 'FOOTER 3')
+		])
+	]);
+
+	console.log(content);
+})();
+
+////////////////////////////////////////////////////////////////////////////////
+// QUOTE
+////////////////////////////////////////////////////////////////////////////////
+
+(() => {
+	console.log('\x1b[36mQUOTE\x1b[0m');
+
+	const content = quote([
+		tag('div', { class: 'class-name' }, [
+			tag('div', {}, 'TEXT')
+		])
+	]);
+
+	console.log(content);
+})();
+
+////////////////////////////////////////////////////////////////////////////////
+// FUNC
+////////////////////////////////////////////////////////////////////////////////
+
+(() => {
+	console.log('\x1b[36mFUNC\x1b[0m');
+
+	const content = func(['data'], quote([
+		tag('div', { class: 'class-name' }, [
+			tag('div', {}, [
+				exp('data')
+			])
+		])
+	]));
+
+	console.log(content);
+})();
+
+////////////////////////////////////////////////////////////////////////////////
+// EXP
+////////////////////////////////////////////////////////////////////////////////
+
+(() => {
+	console.log('\x1b[36mEXP\x1b[0m');
+
+	const content = func(['data'], quote([
+		tag('div', { class: 'class-name' }, [
+			tag('div', {}, [
+				exp('data')
+			])
+		])
+	]));
+
+	console.log(content);
+})();
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPIF
+////////////////////////////////////////////////////////////////////////////////
+
+(() => {
+	console.log('\x1b[36mEXPIF\x1b[0m');
+
+	const content = func(['data'], quote([
+		tag('div', { class: 'class-name' }, [
+			tag('div', {}, [
+				expIf('data', [
+					'data'
+				], [
+					quote('Default Value')
 				])
 			])
 		])
-	])
-];
+	]));
 
-console.log(func(content, ['title']));
+	console.log(content);
+})();
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPLOOP
+////////////////////////////////////////////////////////////////////////////////
+
+(() => {
+	console.log('\x1b[36mEXPLOOP\x1b[0m');
+
+	const content = func(['data'], quote([
+		tag('div', { class: 'class-name' }, [
+			tag('div', {}, [
+				expLoop('let index = 0, length = data.length; index < length; index++', [
+					'data[index]'
+				], ', ')
+			])
+		])
+	]));
+
+	console.log(content);
+})();
